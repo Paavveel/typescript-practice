bc75825d823bb48476687d1364a6167fcf7dfcc7
Paavveel <pa.tomilin@gmail.com>
2022-08-26 17:31:40 +0300

    Enums

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 "use strict";
-const skill = [1, 'Dev'];
-const skills = ['Dev', 'Testing'];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 3] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Payment success',
+    statusCode: StatusCode.SUCCESS,
+};
+// 1 - success
+// 2 - in process
+// 3 - failed
+const action = (status) => { };
+action(StatusCode.SUCCESS);
+action(1);
+const res2 = 1 /* Roles.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,3 +1,26 @@
-const skill: readonly [number, string] = [1, 'Dev'];
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 2,
+  FAILED = 3,
+}
 
-const skills: ReadonlyArray<string> = ['Dev', 'Testing'];
+const res = {
+  message: 'Payment success',
+  statusCode: StatusCode.SUCCESS,
+};
+
+// 1 - success
+// 2 - in process
+// 3 - failed
+
+const action = (status: StatusCode) => {};
+
+action(StatusCode.SUCCESS);
+action(1);
+
+const enum Roles {
+  ADMIN = 1,
+  USER = 2,
+}
+
+const res2 = Roles.ADMIN;
