02d71bb36f10ca0f76942734d418fd29ab9598a6
Paavveel <pa.tomilin@gmail.com>
2022-08-29 12:30:03 +0300

    Literal types

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
 "use strict";
-const logId = (id) => {
-    if (typeof id === 'string') {
-        console.log(id);
-    }
-    else if (typeof id === 'number') {
-        console.log(id);
-    }
-    else {
-        console.log(id);
-    }
+const fetchWithAuth = (url, method) => {
+    return 1;
 };
-logId(1);
-logId('asd');
-logId(true);
-const logError = (err) => {
-    if (Array.isArray(err)) {
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-};
-const logObject = (obj) => {
-    if ('a' in obj) {
-        console.log(obj.a);
-    }
-    else {
-        console.log(obj.b);
-    }
-};
-const logMultipleIds = (a, b) => {
-    if (a === b) {
-        console.log(a);
-    }
-    else {
-        console.log(a);
-    }
-};
-let a = 1;
-// a = 2;
+let method = 'post';
+fetchWithAuth('s', method);
--- app.ts
+++ app.ts
@@ -1,40 +1,7 @@
-const logId = (id: string | number | boolean) => {
-  if (typeof id === 'string') {
-    console.log(id);
-  } else if (typeof id === 'number') {
-    console.log(id);
-  } else {
-    console.log(id);
-  }
+const fetchWithAuth = (url: string, method: 'post' | 'get'): 1 | -1 => {
+  return 1;
 };
 
-logId(1);
-logId('asd');
-logId(true);
+let method = 'post';
 
-const logError = (err: string | string[]) => {
-  if (Array.isArray(err)) {
-    console.log(err);
-  } else {
-    console.log(err);
-  }
-};
-
-const logObject = (obj: { a: number } | { b: number }) => {
-  if ('a' in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
-};
-
-const logMultipleIds = (a: string | number, b: string | boolean) => {
-  if (a === b) {
-    console.log(a);
-  } else {
-    console.log(a);
-  }
-};
-
-let a: 1 = 1;
-// a = 2;
+fetchWithAuth('s', method as 'post');
