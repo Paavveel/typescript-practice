12ad1067d88e1c2b75e10569ccee902635cab04a
Paavveel <pa.tomilin@gmail.com>
2022-09-01 16:03:40 +0300

    Product Cart Example

--- app.js
+++ app.js
@@ -1,28 +1,71 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-class Logger {
-    log(...args) {
-        console.log(...args);
-    }
-    error(...args) {
-        return __awaiter(this, void 0, void 0, function* () {
-            console.log(...args);
-        });
+// Необходимо сделать корзину (Cart) на сайте,
+// которая имеет список продуктов (Product), добавленных в корзину
+// и параметры доставки (Delivery). Для Cart реализовать методы:
+// - Добавить продукт в корзину
+// - Удалить продукт из корзины по ID
+// - Посчитать стоимость товаров в корзине
+// - Задать доставку
+// - Checkout - вернуть что всё ок, если есть продукты и параметры доставки
+// Product: id, название и цена
+// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
+class Product {
+    constructor(id, title, price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
     }
 }
-class User {
-    delete() {
-        throw new Error('Method not implemented.');
-    }
-    pay(paymentId) {
-        ////
+class Delivery {
+    constructor(date) {
+        this.date = date;
     }
 }
+class HomeDelivery extends Delivery {
+    constructor(date, address) {
+        super(date);
+        this.address = address;
+    }
+}
+class ShopDelivery extends Delivery {
+    constructor(shopId) {
+        super(new Date());
+        this.shopId = shopId;
+    }
+}
+class Cart {
+    constructor() {
+        this.products = [];
+    }
+    addProduct(product) {
+        this.products.push(product);
+    }
+    deleteProductById(productId) {
+        this.products = this.products.filter((p) => p.id !== productId);
+    }
+    getSum() {
+        return this.products
+            .map((p) => p.price)
+            .reduce((p1, p2) => p1 + p2);
+    }
+    setDelivery(delivery) {
+        this.delivery = delivery;
+    }
+    checkOut() {
+        if (this.products.length === 0) {
+            throw new Error('Not found any products');
+        }
+        if (!this.delivery) {
+            throw new Error('No delivery');
+        }
+        return { success: true };
+    }
+}
+const cart = new Cart();
+cart.addProduct(new Product(1, 'Cookie', 10));
+cart.addProduct(new Product(2, 'Cake', 30));
+cart.addProduct(new Product(3, 'Banana', 20));
+cart.deleteProductById(1);
+cart.setDelivery(new HomeDelivery(new Date(), 'Paris'));
+console.log(cart.getSum());
+console.log(cart.checkOut());
--- app.ts
+++ app.ts
@@ -1,31 +1,74 @@
-interface ILogger {
-  log(...args: any[]): void;
-  error(...args: any[]): void;
+// Необходимо сделать корзину (Cart) на сайте,
+// которая имеет список продуктов (Product), добавленных в корзину
+// и параметры доставки (Delivery). Для Cart реализовать методы:
+// - Добавить продукт в корзину
+// - Удалить продукт из корзины по ID
+// - Посчитать стоимость товаров в корзине
+// - Задать доставку
+// - Checkout - вернуть что всё ок, если есть продукты и параметры доставки
+// Product: id, название и цена
+// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
+
+class Product {
+  constructor(public id: number, public title: string, public price: number) {}
 }
 
-class Logger implements ILogger {
-  log(...args: any[]): void {
-    console.log(...args);
+class Delivery {
+  constructor(public date: Date) {}
+}
+
+class HomeDelivery extends Delivery {
+  constructor(date: Date, public address: string) {
+    super(date);
   }
-  async error(...args: any[]): Promise<void> {
-    console.log(...args);
+}
+class ShopDelivery extends Delivery {
+  constructor(public shopId: number) {
+    super(new Date());
   }
 }
 
-interface IPayable {
-  pay(paymentId: number): void;
-  price?: number;
+type DeliveryOptions = HomeDelivery | ShopDelivery;
+
+class Cart {
+  private products: Product[] = [];
+  private delivery: DeliveryOptions;
+
+  public addProduct(product: Product): void {
+    this.products.push(product);
+  }
+
+  public deleteProductById(productId: number): void {
+    this.products = this.products.filter((p: Product) => p.id !== productId);
+  }
+
+  public getSum(): number {
+    return this.products
+      .map((p: Product) => p.price)
+      .reduce((p1: number, p2: number) => p1 + p2);
+  }
+
+  public setDelivery(delivery: DeliveryOptions): void {
+    this.delivery = delivery;
+  }
+
+  public checkOut() {
+    if (this.products.length === 0) {
+      throw new Error('Not found any products');
+    }
+    if (!this.delivery) {
+      throw new Error('No delivery');
+    }
+
+    return { success: true };
+  }
 }
 
-interface IDeletable {
-  delete(): void;
-}
-
-class User implements IPayable, IDeletable {
-  delete(): void {
-    throw new Error('Method not implemented.');
-  }
-  pay(paymentId: number | string): void {
-    ////
-  }
-}
+const cart = new Cart();
+cart.addProduct(new Product(1, 'Cookie', 10));
+cart.addProduct(new Product(2, 'Cake', 30));
+cart.addProduct(new Product(3, 'Banana', 20));
+cart.deleteProductById(1);
+cart.setDelivery(new HomeDelivery(new Date(), 'Paris'));
+console.log(cart.getSum());
+console.log(cart.checkOut());
