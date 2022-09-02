78c47d423206d0288156903d37b0d25d8f778190
Paavveel <pa.tomilin@gmail.com>
2022-09-02 12:47:43 +0300

    Functions with generics

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+const logMiddleware = (data) => {
+    console.log(data);
+    return data;
 };
-const nums = [1, 2, 3];
-const test = () => __awaiter(void 0, void 0, void 0, function* () {
-    const a = yield new Promise((resolve, reject) => {
-        resolve(1);
-    });
-});
-const check = {
-    drive: true,
-    transmission: false,
+const res = logMiddleware(10);
+const getSplitedHalf = (data) => {
+    const l = data.length / 2;
+    return data.slice(0, l);
 };
+getSplitedHalf([1, 2, 3]);
--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
-const nums: Array<number> = [1, 2, 3];
-
-const test = async () => {
-  const a = await new Promise<number>((resolve, reject) => {
-    resolve(1);
-  });
+const logMiddleware = <T>(data: T): T => {
+  console.log(data);
+  return data;
 };
 
-const check: Record<string, boolean> = {
-  drive: true,
-  transmission: false,
+const res = logMiddleware<number>(10);
+
+const getSplitedHalf = <T>(data: Array<T>): Array<T> => {
+  const l = data.length / 2;
+
+  return data.slice(0, l);
 };
+
+getSplitedHalf<number>([1, 2, 3]);
