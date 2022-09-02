736265be7428ab239b0263431146716c906bf267
Paavveel <pa.tomilin@gmail.com>
2022-09-02 15:11:08 +0300

    Keyof example with Generic

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 "use strict";
-// Необходимо написать функцию сортировки любых
-// объектов, которые имеют id по убыванию и по возрастанию
-const data = [
-    { id: 2, name: 'Петя' },
-    { id: 1, name: 'Вася' },
-    { id: 3, name: 'Надя' },
-];
-const sort = (data, type = 'asc') => {
-    return data.sort((a, b) => {
-        switch (type) {
-            case 'asc':
-                return a.id - b.id;
-            case 'desc':
-                return b.id - a.id;
-        }
-    });
+const getValue = (obj, key) => obj[key];
+const user = {
+    name: 'Pavel',
+    age: 29,
 };
-console.log(sort(data, 'desc'));
-console.log(sort(data));
+const userName = getValue(user, 'name');
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,26 +1,14 @@
-// Необходимо написать функцию сортировки любых
-// объектов, которые имеют id по убыванию и по возрастанию
-
-const data = [
-  { id: 2, name: 'Петя' },
-  { id: 1, name: 'Вася' },
-  { id: 3, name: 'Надя' },
-];
-
-interface ID {
-  id: number;
+interface IUser {
+  name: string;
+  age: number;
 }
 
-const sort = <T extends ID>(data: T[], type: 'asc' | 'desc' = 'asc'): T[] => {
-  return data.sort((a, b) => {
-    switch (type) {
-      case 'asc':
-        return a.id - b.id;
-      case 'desc':
-        return b.id - a.id;
-    }
-  });
+const getValue = <T, K extends keyof T>(obj: T, key: K) => obj[key];
+
+const user: IUser = {
+  name: 'Pavel',
+  age: 29,
 };
 
-console.log(sort(data, 'desc'));
-console.log(sort(data));
+const userName = getValue(user, 'name');
+console.log(userName);
