0f4d5f7011bc15b3cfce3d2c5059be4c2f17805c
Paavveel <pa.tomilin@gmail.com>
2022-09-02 10:44:11 +0300

    this

--- app.js
+++ app.js
@@ -1,71 +1,27 @@
 "use strict";
-// Необходимо сделать корзину (Cart) на сайте,
-// которая имеет список продуктов (Product), добавленных в корзину
-// и параметры доставки (Delivery). Для Cart реализовать методы:
-// - Добавить продукт в корзину
-// - Удалить продукт из корзины по ID
-// - Посчитать стоимость товаров в корзине
-// - Задать доставку
-// - Checkout - вернуть что всё ок, если есть продукты и параметры доставки
-// Product: id, название и цена
-// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
-class Product {
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-    }
-}
-class Delivery {
-    constructor(date) {
-        this.date = date;
-    }
-}
-class HomeDelivery extends Delivery {
-    constructor(date, address) {
-        super(date);
-        this.address = address;
-    }
-}
-class ShopDelivery extends Delivery {
-    constructor(shopId) {
-        super(new Date());
-        this.shopId = shopId;
-    }
-}
-class Cart {
+class Payment {
     constructor() {
-        this.products = [];
+        this.date = new Date();
+        this.getDateArrow = () => {
+            return this.date;
+        };
     }
-    addProduct(product) {
-        this.products.push(product);
-    }
-    deleteProductById(productId) {
-        this.products = this.products.filter((p) => p.id !== productId);
-    }
-    getSum() {
-        return this.products
-            .map((p) => p.price)
-            .reduce((p1, p2) => p1 + p2);
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkOut() {
-        if (this.products.length === 0) {
-            throw new Error('Not found any products');
-        }
-        if (!this.delivery) {
-            throw new Error('No delivery');
-        }
-        return { success: true };
+    getDate() {
+        return this.date;
     }
 }
-const cart = new Cart();
-cart.addProduct(new Product(1, 'Cookie', 10));
-cart.addProduct(new Product(2, 'Cake', 30));
-cart.addProduct(new Product(3, 'Banana', 20));
-cart.deleteProductById(1);
-cart.setDelivery(new HomeDelivery(new Date(), 'Paris'));
-console.log(cart.getSum());
-console.log(cart.checkOut());
+const p = new Payment();
+const user = {
+    id: 1,
+    paymentDate: p.getDate.bind(p),
+    paymentDateArrow: p.getDateArrow,
+};
+console.log(p.getDate());
+console.log(user.paymentDate());
+console.log(user.paymentDateArrow());
+class PaymentPersistent extends Payment {
+    save() {
+        return this.getDateArrow();
+    }
+}
+console.log(new PaymentPersistent().save());
--- app.ts
+++ app.ts
@@ -1,74 +1,31 @@
-// Необходимо сделать корзину (Cart) на сайте,
-// которая имеет список продуктов (Product), добавленных в корзину
-// и параметры доставки (Delivery). Для Cart реализовать методы:
-// - Добавить продукт в корзину
-// - Удалить продукт из корзины по ID
-// - Посчитать стоимость товаров в корзине
-// - Задать доставку
-// - Checkout - вернуть что всё ок, если есть продукты и параметры доставки
-// Product: id, название и цена
-// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
+class Payment {
+  private date: Date = new Date();
 
-class Product {
-  constructor(public id: number, public title: string, public price: number) {}
+  getDate(this: Payment) {
+    return this.date;
+  }
+
+  getDateArrow = () => {
+    return this.date;
+  };
 }
 
-class Delivery {
-  constructor(public date: Date) {}
-}
+const p = new Payment();
 
-class HomeDelivery extends Delivery {
-  constructor(date: Date, public address: string) {
-    super(date);
-  }
-}
-class ShopDelivery extends Delivery {
-  constructor(public shopId: number) {
-    super(new Date());
-  }
-}
+const user = {
+  id: 1,
+  paymentDate: p.getDate.bind(p),
+  paymentDateArrow: p.getDateArrow,
+};
 
-type DeliveryOptions = HomeDelivery | ShopDelivery;
+console.log(p.getDate());
+console.log(user.paymentDate());
+console.log(user.paymentDateArrow());
 
-class Cart {
-  private products: Product[] = [];
-  private delivery: DeliveryOptions;
-
-  public addProduct(product: Product): void {
-    this.products.push(product);
-  }
-
-  public deleteProductById(productId: number): void {
-    this.products = this.products.filter((p: Product) => p.id !== productId);
-  }
-
-  public getSum(): number {
-    return this.products
-      .map((p: Product) => p.price)
-      .reduce((p1: number, p2: number) => p1 + p2);
-  }
-
-  public setDelivery(delivery: DeliveryOptions): void {
-    this.delivery = delivery;
-  }
-
-  public checkOut() {
-    if (this.products.length === 0) {
-      throw new Error('Not found any products');
-    }
-    if (!this.delivery) {
-      throw new Error('No delivery');
-    }
-
-    return { success: true };
+class PaymentPersistent extends Payment {
+  save() {
+    return this.getDateArrow();
   }
 }
 
-const cart = new Cart();
-cart.addProduct(new Product(1, 'Cookie', 10));
-cart.addProduct(new Product(2, 'Cake', 30));
-cart.addProduct(new Product(3, 'Banana', 20));
-cart.deleteProductById(1);
-cart.setDelivery(new HomeDelivery(new Date(), 'Paris'));
-console.log(cart.getSum());
-console.log(cart.checkOut());
+console.log(new PaymentPersistent().save());
